5381057ed90110cdb578f7a873cf74b140f24341
Raul Porras <gibran.porras22@gmail.com>
2020-06-24 12:49:39 -0600

    Updates

---
+++ precache-manifest.5403df379979dd60d3619ea6c7800dba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d91b2ada556de66d7f8b32b482a5a8",
+    "url": "/theoldbarber/index.html"
+  },
+  {
+    "revision": "c408506f46181ccd5a45",
+    "url": "/theoldbarber/static/css/2.e603e8eb.chunk.css"
+  },
+  {
+    "revision": "bbbfa71c84166da97a28",
+    "url": "/theoldbarber/static/css/main.31e93772.chunk.css"
+  },
+  {
+    "revision": "c408506f46181ccd5a45",
+    "url": "/theoldbarber/static/js/2.a5422684.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/theoldbarber/static/js/2.a5422684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbbfa71c84166da97a28",
+    "url": "/theoldbarber/static/js/main.476e2fa1.chunk.js"
+  },
+  {
+    "revision": "7875356551922de46361",
+    "url": "/theoldbarber/static/js/runtime-main.cefc9ba6.js"
+  }
+]);
